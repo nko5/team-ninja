891c7f03573a69148c70d2e77325d6a32f4ef9ba
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2015-11-07 15:02:53 +0530

    Added game api

--- server/api/game/game.model.js
+++ server/api/game/game.model.js
@@ -4,9 +4,33 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var GameSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+    name: String,
+    host: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    players: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    dateCreated: {type: Number, default: Date.now},
+    lastUpdated: {type: Number, default: Date.now},
+    dateStarted: {type: Number, default: Date.now},
+    dateEnded: {type: Number, default: Date.now},
+    calledNumbers: Array,
+    rules: [{
+        id: {
+            type: Schema.Types.ObjectId,
+            ref: 'Rule'
+        },
+        name: String,
+        wonBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }],
+    info: String,
+    active: Boolean
 });
 
 module.exports = mongoose.model('Game', GameSchema);
