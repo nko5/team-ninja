bf50048530dbdcde6a79e4ec1b66d4ba7d3e6997
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2015-11-07 23:23:39 +0530

    Added handling for sockets

--- client/app/projector/projector.controller.js
+++ client/app/projector/projector.controller.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('teamNinjaApp')
-    .controller('ProjectorCtrl', function (Veronica, GameApi, $interval,$scope,User,Auth,$timeout) {
+    .controller('ProjectorCtrl', function (Veronica, GameApi, $interval, $scope, User, Auth, $timeout, SocketIO) {
         var self = this;
         self.number;
         $scope.showMessage = false;
 
         $scope.getCurrentUser = Auth.getCurrentUser;
 
-        GameApi.list(function(data){
+        GameApi.list(function (data) {
             self.game = data.games[0];
         });
 
@@ -25,7 +25,7 @@ angular.module('teamNinjaApp')
         };
 
         var callNumber = function () {
-            GameApi.callNumber({id: self.game._id}, function(number){
+            GameApi.callNumber({id: self.game._id}, function (number) {
                 self.number = number;
                 Veronica.say(number.description, [{
                     delay: 1000,
@@ -34,21 +34,81 @@ angular.module('teamNinjaApp')
             });
         };
 
-    $scope.sendInvite = function(user){
-      console.log(user);
-      if(user){
-        User.invitePlayer({sender : user.email, receiver : $scope.inviteEmail, gameId:self.game._id}, function(result){
-          $scope.showMessage = true;
-          if(result.sent) {
-            $scope.alertMessage = "Invite sent successfully.";
-          }
-          else{
-            $scope.alertMessage = "Error sending invite. Please try again later.";
-          }
-          $timeout(function(){
-            $scope.showMessage = false;
-          },3000);
+        SocketIO.socket.on("JOIN", function (data) {
+            self.game.players.push(data.user);
         });
-      }
-    };
+
+        SocketIO.socket.on("LEAVE", function (data) {
+            var userToRemove;
+            self.game.players.forEach(function(user, index){
+                if(user._id == data.userId){
+                    userToRemove = index;
+                }
+            });
+            self.game.players.splice(userToRemove, 1);
+        });
+
+        SocketIO.socket.on("CLAIM", function (data) {
+            var userToUpdate;
+            self.game.players.forEach(function(user){
+                if(user._id == data.userId){
+                    userToUpdate = user;
+                }
+            });
+
+            userToUpdate.message = data.rule.name;
+
+            $timeout(function(){
+                if(data.rule.name == userToUpdate.message){
+                    userToUpdate.message = "";
+                }
+            }, 2000);
+        });
+
+        SocketIO.socket.on("CHAT", function (data) {
+            var userToUpdate;
+            self.game.players.forEach(function(user){
+                if(user._id == data.userId){
+                    userToUpdate = user;
+                }
+            });
+
+            userToUpdate.message = data.message;
+
+            $timeout(function(){
+                if(data.message == userToUpdate.message){
+                    userToUpdate.message = "";
+                }
+                userToUpdate.message = "";
+            }, 2000);
+        });
+
+        $scope.$on("$destroy", function () {
+            socket.removeAllListeners("JOIN");
+            socket.removeAllListeners("LEAVE");
+            socket.removeAllListeners("CLAIM");
+            socket.removeAllListeners("CHAT");
+        });
+
+        $scope.sendInvite = function (user) {
+            console.log(user);
+            if (user) {
+                User.invitePlayer({
+                    sender: user.email,
+                    receiver: $scope.inviteEmail,
+                    gameId: self.game._id
+                }, function (result) {
+                    $scope.showMessage = true;
+                    if (result.sent) {
+                        $scope.alertMessage = "Invite sent successfully.";
+                    }
+                    else {
+                        $scope.alertMessage = "Error sending invite. Please try again later.";
+                    }
+                    $timeout(function () {
+                        $scope.showMessage = false;
+                    }, 3000);
+                });
+            }
+        };
     });
