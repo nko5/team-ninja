2579fba36b6c6fd38693697f6bff89dac180d4ea
Manoj Nama <manoj.nama@outlook.com>
2015-11-08 14:36:29 +0530

    Updated the UI for login page

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,7 +224,7 @@ module.exports = function (grunt) {
       target: {
         src: '<%= yeoman.client %>/home.html',
         ignorePath: '<%= yeoman.client %>/',
-        exclude: ['/json3/', '/es5-shim/', /font-awesome.css/ ]
+        exclude: ['/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/ ]
       }
     },
 
